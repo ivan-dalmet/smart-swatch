943eb96233c4cfec7f672601596e1384fdd39297
Ivan Dalmet <ivan@dalmet.fr>
2019-10-02 13:15:11 +0200

    feat: Add feature to change name before sketch palette download

--- src/components/SaveSketchPalette.js
+++ src/components/SaveSketchPalette.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 // import PropTypes from 'prop-types';
-import { Button } from '@chakra-ui/core';
+import {
+  Input,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  FormControl,
+  FormLabel,
+  PopoverArrow,
+  Stack,
+} from '@chakra-ui/core';
+import filenamify from 'filenamify';
 import FileSaver from 'file-saver';
 
 const propTypes = {};
 const defaultProps = {};
 
+const defaultName = 'color';
+
 export const SaveSketchPalette = ({ colors }) => {
+  const initialFocusRef = useRef();
+  const [colorName, setColorName] = useState(defaultName);
   const fileContent = `
   {
     "compatibleVersion":"2.0",
@@ -14,7 +30,7 @@ export const SaveSketchPalette = ({ colors }) => {
     "colors":[
       ${colors.map((color, i) => `
         {
-          "name": "Color ${(i + 1) * 100}",
+          "name": "${colorName || defaultName} ${(i + 1) * 100}",
           "red": ${color.get('rgb.r')/255},
           "green": ${color.get('rgb.g')/255},
           "blue": ${color.get('rgb.b')/255},
@@ -26,11 +42,44 @@ export const SaveSketchPalette = ({ colors }) => {
   `;
 
   const blob = new Blob([fileContent], {type: "text/plain;charset=utf-8"});
+  const filename = colorName ? `${colorName.toLowerCase()}-` : '';
 
   return (
-    <Button size="xs" onClick={() => { FileSaver.saveAs(blob, "swatch.sketchpalette"); }}>
-      Save as .sketchpalette file
-    </Button>
+    <Popover placement="bottom" initialFocusRef={initialFocusRef}>
+      <PopoverTrigger>
+        <Button size="xs">
+          Save as .sketchpalette file
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent zIndex={4}>
+        <PopoverArrow />
+        <PopoverBody
+          as="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            FileSaver.saveAs(blob, filenamify(`${filename}swatch.sketchpalette`));
+            setColorName(defaultName);
+          }}
+        >
+          <FormControl>
+            <FormLabel htmlFor="sketch-palette-color-name">
+              Color name
+            </FormLabel>
+            <Stack isInline>
+              <Input
+                id="sketch-palette-color-name"
+                ref={initialFocusRef}
+                value={colorName}
+                onChange={e => setColorName(e.target.value)}
+              />
+              <Button flex="none">
+                Download
+              </Button>
+            </Stack>
+          </FormControl>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
   );
 };
 
